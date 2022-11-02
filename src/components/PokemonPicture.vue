28560e982d9400e09984243f2ab40e0410168aee
Erik Mendez <guillen4473@gmail.com>
2022-11-01 21:36:43 -0600

    hola mundo

---
+++ src/components/PokemonPicture.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="pokemon-container"></div>
+    <img
+        v-if="showPokemon"
+        :src="imgSrc"
+        class="fade-in"
+        alt="pokemon">   
+    <img
+        v-else
+        :src="imgSrc"
+        class="hidden-pokemon"
+        alt="pokemon">
+  </template>
+<script>
+export default {
+    props: {
+        pokemonId: {
+            type: Number,
+            required: true
+        },
+        showPokemon: {
+            type: Boolean,
+            require: true,
+            default: false
+        }
+    },
+    computed: {
+        imgSrc() {
+            return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${this.pokemonId}.svg`
+        }
+    }
+}
+</script>
+<style scoped>
+/* Pokemon Picture */
+
+img {
+    height: 200px;
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+}
+
+.hidden-pokemon {
+    filter: brightness(0);
+}
+
+
+
+
+</style>
+
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,62 @@
+<template>
+    <h1 v-if="!pokemon">Espere por favor...</h1>
+    <div v-else="pokemon">
+        <!--TODO: paragraph-->
+        <h1>Quien es este pokemon?</h1>
+        <!--TODO: Img-->
+        <PokemonPicture :pokemonId="pokemon.id" :showPokemon="showPokemon" />
+        <!--TODO: Options-->
+        <PokemonOptions @selection="checkAnswer" :pokemons="pokemonArr" />
+        <template v-if="showAnswer">
+            <h2 class="fade-in">{{message}}</h2>
+            <button @click="reiniciar">Nuevo juego</button>
+        </template>
+
+    </div>
+</template>
+<script>
+import PokemonPicture from '@/components/PokemonPicture.vue';
+import PokemonOptions from '@/components/PokemonOptions.vue';
+import getPokemonOptions from '@/helpers/getPokemonOptions'
+
+
+export default {
+    components: { PokemonPicture, PokemonOptions },
+    data() {
+        return {
+            pokemonArr: [],
+            pokemon: null,
+            showPokemon: false,
+            showAnswer: false,
+            message: ''
+        }
+    },
+    methods: {
+        async mixPokemonArray() {
+            this.pokemonArr = await getPokemonOptions()
+            const rndInt = Math.floor(Math.random() * 4)
+            this.pokemon = this.pokemonArr[rndInt]
+        },
+        checkAnswer(pokemonId) {
+            this.showPokemon = true
+            if (this.pokemon.id == pokemonId) {
+                this.message = `Correcto, es ${this.pokemon.name}`
+            } else {
+                this.message = `Oops, era ${this.pokemon.name}`
+            }
+            this.showAnswer = true
+        },
+        reiniciar() {
+            this.showPokemon = false
+            this.showAnswer = false
+            this.pokemonArr = []
+            this.pokemon = null
+            this.mixPokemonArray()
+        }
+    },
+    mounted() {
+        this.mixPokemonArray()
+    }
+
+}
+</script>
